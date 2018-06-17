f8a12ddee413dfb5bb6b705a0a3afaec5046c8f3
CynthiaXXDB <37497306+CynthiaXXDB@users.noreply.github.com>
2018-06-17 23:44:47 +0800

    登陆

--- kczdcsxt/login_submit.php
+++ kczdcsxt/login_submit.php
@@ -1,30 +1,5 @@
 <?php 
     include("conn.php");
-    $sql=mysql_query(" SELECT * FROM student");
-    $user=$_POST[user];
-    $psd=$_POST[password];
-    echo $user,$psd;
-    while($abc = mysql_fetch_assoc($sql))
-    {
-        $i=0;
-        if($user==$abc['user'])
-        {
-          $i=1;
-          if($psd==$abc['password'])
-          {
-            echo "登陆成功";
-            header ("Location: index.php?id=".$user);
-          }
-          else
-          {
-            echo "密码错误";
-          }
-        }
-        if($i==0)
-        {
-          echo "用户名错误";
-        }
-     }
     $quanxian=$_POST[quanxian];
     $user=$_POST[user];
     $psd=$_POST[password];
@@ -75,7 +50,6 @@
                 }
                 else
                 {
-                    echo "密码错误";
                     echo "<script language=\"JavaScript\">\r\n";
                     echo " alert(\"密码错误\");\r\n";
                     echo " history.back();\r\n";
@@ -117,7 +91,6 @@
             }
             if($i==0)
             {
-                echo "用户名不存在";
                 echo "<script language=\"JavaScript\">\r\n";
                 echo " alert(\"用户名不存在\");\r\n";
                 echo " history.back();\r\n";
