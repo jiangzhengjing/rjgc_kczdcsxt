76399dcf6653735eb1b3f5960daffa5c6bd6e6da
CynthiaXXDB <37497306+CynthiaXXDB@users.noreply.github.com>
2018-06-17 23:19:54 +0800

    登陆
    加权限

---
+++ kczdcsxt/login.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<title>课程自动检测系统</title>
+<link href="style/css/loginstyle.css" rel="stylesheet" type="text/css">
+
+</head>
+
+<body class="login">
+
+<div class="login_m">
+	
+	<div class="login_logo" style="font-size: 34px;">课程自动检测系统</div>
+	<div class="login_boder">
+		<div class="login_padding">
+		 <form role="form" action="login_submit.php" method="post">
+			<label>用户名</label>
+			
+				<input type="text" id="username" name="user" class="txt_input txt_input2" onfocus="if (value ==&#39;Your name&#39;){value =&#39;&#39;}" onblur="if (value ==&#39;&#39;){value=&#39;Your name&#39;}" value="Your name">
+				
+		
+			<label>密码</label>
+			
+				<input type="password" name="password" id="userpwd" class="txt_input" onfocus="if (value ==&#39;******&#39;){value =&#39;&#39;}" onblur="if (value ==&#39;&#39;){value=&#39;******&#39;}" value="******">
+			
+			
+			<div class="rem_sub">
+				<div class="rem_sub_l">
+					<input type="radio" name="quanxian" id="save_me" value="admin">
+					<label for="radio">管理员</label>
+					<input type="radio" name="quanxian" id="save_me" value="teacher">
+					<label for="radio">教师</label>
+					<input type="radio" name="quanxian" id="save_me" checked value="student">
+					<label for="radio">学生</label>
+				</div>
+				<label>
+					<input type="submit" class="sub_button" name="button" id="button" value="登录" style="opacity: 0.7;">
+<!--					<button type="submit" class="sub_button" style="opacity: 0.7;">登录</button>-->
+				</label>
+			</div>
+			</form>
+		</div>
+	</div><!--login_boder end-->
+</div><!--login_m end-->
+
+<br />
+<br />
+
+
+
+</body>
+</html>
---
+++ kczdcsxt/login_submit.php
@@ -0,0 +1,108 @@
+<?php 
+    include("conn.php");
+
+    $quanxian=$_POST[quanxian];
+    $user=$_POST[user];
+    $psd=$_POST[password];
+    if($quanxian=="admin")
+    {
+        $sql=mysql_query(" SELECT * FROM admin");
+        while($abc = mysql_fetch_assoc($sql))
+        {
+            $i=0;
+            if($user==$abc['user'])
+            {
+                $i=1;
+                if($psd==$abc['password'])
+                {
+                    header ("Location: admin/index.php?id=".$user);
+                }
+                else
+                {
+                    echo "<script language=\"JavaScript\">\r\n";
+                    echo " alert(\"密码错误\");\r\n";
+                    echo " history.back();\r\n";
+                    echo "</script>";
+                    exit;
+                }
+            }
+            if($i==0)
+            {
+                  echo "<script language=\"JavaScript\">\r\n";
+                  echo " alert(\"用户名不存在\");\r\n";
+                  echo " history.back();\r\n";
+                  echo "</script>";
+                  exit;
+            }
+        }
+    }
+    else if($quanxian=="teacher")
+    {
+        $sql=mysql_query(" SELECT * FROM teacher");
+        while($abc = mysql_fetch_assoc($sql))
+        {
+            $i=0;
+            if($user==$abc['user'])
+            {
+                $i=1;
+                if($psd==$abc['password'])
+                {
+                    header ("Location: teacher/index.php?id=".$user);
+                }
+                else
+                {
+                    echo "密码错误";
+                    echo "<script language=\"JavaScript\">\r\n";
+                    echo " alert(\"密码错误\");\r\n";
+                    echo " history.back();\r\n";
+                    echo "</script>";
+                    exit;
+                }
+            }
+            if($i==0)
+            {
+                 echo "<script language=\"JavaScript\">\r\n";
+                 echo " alert(\"用户名不存在\");\r\n";
+                 echo " history.back();\r\n";
+                 echo "</script>";
+                 exit;
+            }
+        }
+    }
+    else if($quanxian=="student")
+    {
+        $sql=mysql_query(" SELECT * FROM student");
+        while($abc = mysql_fetch_assoc($sql))
+        {
+            $i=0;
+            if($user==$abc['user'])
+            {
+                $i=1;
+                if($psd==$abc['password'])
+                {
+                    header ("Location: student/index.php?id=".$user);
+                }
+                else
+                {
+                    echo "<script language=\"JavaScript\">\r\n";
+                    echo " alert(\"密码错误\");\r\n";
+                    echo " history.back();\r\n";
+                    echo "</script>";
+                    exit;
+                }
+            }
+            if($i==0)
+            {
+                echo "用户名不存在";
+                echo "<script language=\"JavaScript\">\r\n";
+                echo " alert(\"用户名不存在\");\r\n";
+                echo " history.back();\r\n";
+                echo "</script>";
+                exit;
+            }
+        }
+    }
+    
+    
+    mysql_close();
+?>
